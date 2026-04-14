991597b2e46ba2310e205fc9bf0c3482d6b4883d
PBehague <114167055+QACS-TL@users.noreply.github.com>
2026-04-14 12:04:19 +0100

    Add files via upload

---
+++ LabSols/Lab1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LabsSolutions.Lab1;
+public class Program
+{
+    public static void Main()
+    {
+        // Console.WriteLine("Hello, World!");
+        var lifeInsurance = new LifeInsurance();
+        double money = lifeInsurance.Calculate();
+        Console.WriteLine($"Please pay ${money} each month.");
+    }
+}
